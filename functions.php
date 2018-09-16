fe2292118a6057f4e6c67b90a03b163e2fc8338a
MymenTech <mymentech@gmail.com>
2018-09-16 16:25:35 +0600

    Enabled parent style.

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function lucille_child_theme_setup(){
 add_action('after_setup_theme','lucille_child_theme_setup');
 
 function lucille_child_scripts(){
-	//wp_enqueue_style('parent-style', get_parent_theme_file_path('/style.css'));
+	wp_enqueue_style('parent-style', get_parent_theme_file_path('/style.css'));
 }
 add_action('wp_enqueue_scripts','lucille_child_scripts');
 
